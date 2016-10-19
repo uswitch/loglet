ed0235960c1174c1091f20e8323268c1d3ff480a
sdawara <siddharthdawara@gmail.com>
2016-10-19 11:23:05 +0100

    Renaming ExtraFields to DefaultFields to strongly indicate the purpose ;)

--- transformers/default.go
+++ transformers/default.go
@@ -4,17 +4,17 @@ import (
 	"github.com/uswitch/loglet/types"
 )
 
-type ExtraFields struct {
+type DefaultFields struct {
 	Fields map[string]string
 }
 
-func NewExtraFields(fields map[string]string) *ExtraFields {
-	return &ExtraFields{
+func NewDefaultFields(fields map[string]string) *DefaultFields {
+	return &DefaultFields{
 		Fields: fields,
 	}
 }
 
-func (p *ExtraFields) Transform(m *types.LogMessage) {
+func (p *DefaultFields) Transform(m *types.LogMessage) {
 	for k, v := range p.Fields {
 		_, ok := m.Fields[k]
 		if !ok {
